c9932296373edaeef8eee4ad42b9deb213ef16f6
koreclyk <camillekamai37@gmail.com>
2026-01-24 15:18:31 -0800

    Create TryingMe class for user info formatting
    v1 of a practice program to precede receipts
    
    added a class, used public static String/return and public static void/Scanner to receive user input
    
    does not spit anything out

---
+++ tryingSomething.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class TryingMe{
+
+   public static String formatInfo(String fname, int fNum, float eGPA){
+      return String.format("First Name: %s\nNumber: %d\nExpected GPA: %.2f",
+                           fname, fNum, eGPA);
+   
+   }
+   
+   
+   public static void main (String[] args){
+      Scanner in = new Scanner(System.in);
+      
+      String fname;
+      int faveNum;
+      float expectedGPA;
+      
+      System.out.print("Enter your first name: ");
+      fname = in.next();
+      
+      System.out.print("Enter your favorite whole number: ");
+      faveNum = in.nextInt();
+      
+      System.out.print("Enter your expected GPA for this class: ");
+      expectedGPA = in.nextFloat();
+      
+      String formattedStr = formatInfo(fname, faveNum, expectedGPA);
+      
+      System.out.println();
+      
+   }
+}
