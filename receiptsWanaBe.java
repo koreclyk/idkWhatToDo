5146a5b2bd4011f7d52c90810a18acc2553e5eeb
koreclyk <camillekamai37@gmail.com>
2026-01-25 10:38:22 -0800

    Just print
    JUST PRINT

---
+++ receiptsWanaBe.java
@@ -0,0 +1,30 @@
+// receipts rough draft
+
+/*
+RELEVANT INFO:
+System.out.print() >>>prints output
+methods & classes
+DECLARING AND STORING VALUES: int/double/String
+operations
+receiving user input: import java.util.Scanner
+                      public static void main (String[] args){
+                        Scanner in = new Scanner(System.in);
+reading inputs
+output formatting: printf(%.2f)
+*/
+
+/*
+import java.util.Scanner;
+
+public class receiptsWanaBe{
+
+  public static String formatInfo(String
+*/
+
+//just print
+public class receiptsWanaBe{
+  
+  public static void main(String[] args){
+    System.out.print("Whats good homes!");
+  }
+}
