1e00a7e0053200702689efe5fbdc539b7032ad4a
koreclyk <camillekamai37@gmail.com>
2026-01-25 11:43:20 -0800

    Refactor formatInfo and main methods in TryingMe class
    trying to recreate the example but I'm realizing, as I input the code, the output will not look the same as the example with the direction I'm taking. Taking a break to re-read info.

--- tryingSomething.java
+++ tryingSomething.java
@@ -1,13 +1,31 @@
+//TRY THIS AGAIN
+import java.util.Scanner;
+
+class TryMe{
+
+   public static String formatInfo(String iname, double iprice, int iqty){
+      return String.format("-----Belethor's Goods-----\nCashier: Belethor\t\tDate: 09/18/2023\n\nQTY\tDesc\t\t\tAmt\n"%d\t%s\t\t\t%d",
+                           iqty, iname, iprice);
+   }
+
+   public static void main (String[] args){
+      Scanner in = new Scanner(System.in);
+
+      String iname;
+      double iprice;
+      int iqty;
+
+      System.out.print("Enter the number of
+
+/*
 import java.util.Scanner;
 
 class TryingMe{
 
    public static String formatInfo(String fname, int fNum, float eGPA){
-      return String.format("First Name: %s\nNumber: %d\nExpected GPA: %.2f",
+      return String.format("First Name: %s\nNumber: %d\nExpected GPA: %.2f", //this will tell the system to give me a result
                            fname, fNum, eGPA);
-      System.out.printf("First Name: %s\nNumber: %d\nExpected GPA: %.2f",
-                           fname, fNum, eGPA);
-   
+   //I had a print statement here thinking it would print my result but that needs to print the "formattedStr" item to get the results
    }
    
    
@@ -32,10 +50,9 @@ class TryingMe{
       
       System.out.println();
       formatInfo(fname, faveNum, expectedGPA);
+
+      System.out.println(formattedStr); //prints the received input
       
    }
 }
-
-/* is this working? >>> no, it's not (trying to see if the file will update on desktop)
-it isn't updating in real time?
 */
