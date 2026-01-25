270a05b8493dabc75e31c436a589d7d4a2c031fd
koreclyk <camillekamai37@gmail.com>
2026-01-25 10:58:19 -0800

    Printing
    literally just realized the prices need to be stored separately from the names of the items so the system knows to perform math on the numbers ONLY

--- receiptsWanaBe.java
+++ receiptsWanaBe.java
@@ -21,10 +21,34 @@ public class receiptsWanaBe{
   public static String formatInfo(String
 */
 
+
+
+
 //just print
+/*
+//this isn't going to work because the numeric value has to be stored in a way that the system can read them solo
+public class receiptsWanaBe{
+  public static void main(String[] args){
+    System.out.print("---Soups, Stews, All Kinds of Brews!---");
+    System.out.println("\nCarrot: $0.98 per piece");
+    System.out.println("Potato: $1.10 per piece");
+    System.out.println("Ginger: $2.00 per piece");
+    System.out.println("Celery: $0.50 per stalk");
+    System.out.println("Onion: $1.25 per piece");
+    System.out.println("Tomato: $1.15 per piece");
+    System.out.println("Beans: $1.60 per pound");
+    System.out.println("Cabbage: $2.00 per head");
+    System.out.println("Broth: BEEF  $1.00 per cup\nCHICKEN  $0.85 per cup");
+    System.out.println("Meat: BEEF  $5.00 per pound\nPORK  $3.75 per pound\nCHICKEN  $3.00 per pound\nFISH  $2.90 per pound");
+  }
+}
+*/
+
+/*
 public class receiptsWanaBe{
   
   public static void main(String[] args){
     System.out.print("Whats good homes!");
   }
 }
+*/
