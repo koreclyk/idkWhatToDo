7756b9668daa3a817f1430d71ca07685818bef33
koreclyk <camillekamai37@gmail.com>
2026-01-28 06:03:39 -0800

    TOTAL rework
    Updated THE WHOLE THING
    
    To find out I've been storing floats when my math values need doubles BUT WE'RE ALMOST THERE
    
    I gtg

--- receiptsWanaBe.java
+++ receiptsWanaBe.java
@@ -23,16 +23,63 @@ class ReceiptsWanaBe{
       Scanner in = new Scanner(System.in);
       
       float pototprice = 1.1f;
-      float cacarotprice = 0.98f;
+      float cacarotprice = 6000.0f;
       float gingarprice = 0.75f;
-      float brothprice = 3.0f;
+      float broprice = 3.0f;
       float meatprice = 4.0f;
-      
+
+      System.out.print("\n~~~Soups, Stews, and all Kinds of Brews!~~~\n\n");
+      System.out.print("\t\t\tWelcome customer!\n\n");
+        
+     //adding spaces so they come out straight
       System.out.printf("Potatoes:\t\t$%.2f\n", pototprice);
-      System.out.printf("Carrots:\t\t$%.2f\n", cacarotprice);
-      System.out.printf("Ginger:\t\t$%.2f\n", gingarprice);
-      System.out.printf("Broth(s):\t\t$%.2f\n", brothprice);
-      System.out.printf("Meat(s):\t\t$%.2f", meatprice);
+      System.out.printf("Carrots:\t\t   $%.2f\n", cacarotprice);
+      System.out.printf("Ginger:\t\t   $%.2f\n", gingarprice);
+      System.out.printf("Broth(s):\t\t$%.2f\n", broprice);
+      System.out.printf("Meat(s):\t\t   $%.2f", meatprice);
+
+      System.out.println("\n\nHow many potatoes would you like?: ");
+      float pototqty = in.nextFloat();
+      System.out.println("How many carrots would you like?: ");
+      float cacarotqty = in.nextFloat();
+      System.out.println("How much ginger would you like?: ");
+      float gingarqty = in.nextFloat();
+      System.out.println("How much broth would you like?: ");
+      float broqty = in.nextFloat();
+      System.out.println("How much meat would you like?: ");
+      float meatqty = in.nextFloat();
+
+      float pototpur = pototprice * pototqty;
+      float cacarotpur = cacarotprice * cacarotqty;
+      float gingarpur = gingarprice * gingarqty;
+      float bropur = broprice * broqty;
+      float meatpur = meatprice * meatqty;
+
+      System.out.println("\n\nQuantity\t\tItem\t\t\t\tCost\n");
+      System.out.printf("%.0f\t\t\t\tPotato(es)\t\t$%.2f", pototqty, pototpur);
+      System.out.printf("\n%.0f\t\t\t\tCarrot(s)\t\t$%.2f", cacarotqty, cacarotpur);
+      System.out.printf("\n%.0f\t\t\t\tGinger\t\t\t$%.2f", gingarqty, gingarpur);
+      System.out.printf("\n%.0f\t\t\t\tBroth(s)\t\t\t$%.2f", broqty, bropur);
+      System.out.printf("\n%.0f\t\t\t\tMeat(s)\t\t\t$%.2f", meatqty, meatpur);
+
+      float subtot = pototpur + cacarotpur + gingarpur + bropur + meatpur;
+      float tax = subtot * 0.85;
+      float toats = subtot + tax;
+
+      System.out.printf("\n\nSubtotal:\t\t$.2f", subtot);
+      System.out.printf("\nTax:\t\t$.2f", tax);
+      System.out.printf("\nTotal:\t\t$.2f", toats);
+
+      System.out.print("How much will you be paying with today?: ");
+      float custin = in.nextFloat();
+
+      float custdone = toats - custin;
+
+      System.out.printf("Remaining: $%.2f", custdone);
+      System.out.print("\n\n\tThank you for shopping at Soups, Stews, and all Kinds of Brews!");
+      System.out.print("\n\tSee you next time!");
+     
+      
       
    }
 }
@@ -145,3 +192,4 @@ class ReceiptsWanaBe{
   }
 }
 */
+
